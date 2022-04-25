2905b0e2fee10f9012dcc96a7108e385e195c2aa
abschill <ajay198810@gmail.com>
2022-04-24 22:07:40 -0600

    update: outpath option@0.1.2

--- bin/index.js
+++ bin/index.js
@@ -1,21 +1,34 @@
 #!/usr/bin/env node
-const { watch, readFileSync, writeFileSync } = require('fs');
+const { watch, readFileSync, writeFileSync, existsSync, mkdirSync } = require('fs');
 const { join, resolve } = require('path');
 const { charset_default, css_ext, less_ext, path_default } = require('./enums');
 const less = require('less');
 const [,,
     watchPath = path_default, 
+    outPath = '.',
     encoding = charset_default
 ] = process.argv;
 const { log } = console;
 const wpLink = join(process.cwd(), watchPath)
+const writeDir = join(process.cwd(), outPath);
+if(!existsSync(writeDir)) {
+    mkdirSync(writeDir);
+    
+}
 watch(wpLink, { recursive: true, encoding }, ( _, fileName) => {
     const filePath = resolve(wpLink, fileName);
     const fileContent = readFileSync(filePath).toString(encoding);
     if(!fileName.includes(less_ext)) return;
     // render it brah
     less.render(fileContent).then( content => {
-        writeFileSync(filePath.replace(less_ext, css_ext), content.css);
+        if(outPath !== '.') {
+            const writePath = resolve(writeDir, fileName.replace(less_ext, css_ext));
+            writeFileSync(writePath, content.css);
+        }
+        else {
+            writeFileSync(filePath.replace(less_ext, css_ext), content.css);
+        }
+        
         log(`\x1b[32mStyles Updated\x1b[0m: ${fileName}\n
 Full Path: \x1b[35m${filePath}\x1b[0m`);
     });
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 # lesscss-watch
 
 it watches lesscss for changes..
+
+argv[2] is the directory to watch
+argv[3] is the outpath (defaults cwd, next to original file, uses same name replaces extension to .css)
